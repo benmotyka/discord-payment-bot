c8355d2c945751cea53a1be4d1e65c3bc05332dc
ben <rikenpl99@gmail.com>
2022-02-01 23:41:15 +0100

    basic files structure

--- app.ts
+++ app.ts
@@ -1,5 +1,7 @@
 import DiscordJS, { Intents, ClientOptions } from 'discord.js'
 import dotenv from 'dotenv'
+import checkVoucher from "./commands/checkVoucher"
+import redeemVoucher from './commands/redeemVoucher'
 dotenv.config()
 
 const options: ClientOptions = {
@@ -18,56 +20,24 @@ client.on('ready', () => {
     const guild = client.guilds.cache.get(testGuildId)
     const commands = guild ? guild.commands : client.application?.commands
 
-    commands?.create({
-        name: 'ping',
-        description: 'Replies with pong'
-    })
-
-    commands?.create({
-        name: 'checkvoucher',
-        description: 'Checks status of voucher',
-        options: [ //arguments
-            {
-                name: 'voucher',
-                description: '32-digit voucher code',
-                required: true,
-                type: DiscordJS.Constants.ApplicationCommandOptionTypes.STRING
-            }
-        ]
+    checkVoucher({
+        commands,
+        client
+    });
+    redeemVoucher({
+        commands,
+        client
     })
 })
 
-client.on('messageCreate', (message) => {
-    if (message.content === 'ping') {
-        message.react('🤣')
-        message.reply({
-            content: 'pong'
-        })
-    }
-})
+// client.on('messageCreate', (message) => {
+//     if (message.content === 'ping') {
+//         message.react('🤣')
+//         message.reply({
+//             content: 'pong'
+//         })
+//     }
+// })
 
-client.on('interactionCreate', async (interaction) => {
-    if (!interaction.isCommand()) return
-    const {commandName, options} = interaction
-    if (commandName === 'ping') interaction.reply({
-        content: 'pong',
-        ephemeral: true //only user who uses command can see it
-    })
-    else if (commandName === 'checkvoucher') {
-        const voucher = options.getString('voucher')
-        //const status = await axios.get('api/${voucher}')
-        //const transactionHash = status.txHash
-        const transactionHash = 'abcdef'
-        await interaction.deferReply({
-            ephemeral: true
-        })
-
-        await new Promise(resolve => setTimeout(resolve, 5000))
-
-        await interaction.editReply({
-            content: `The transaction hash of your voucher is: ${transactionHash}`
-        })
-    }
-})
 
 client.login(process.env.TOKEN)
