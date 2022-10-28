adeea8ffbd1da6e1e83b3275059beb8623dcc637
ben <benmotykax@gmail.com>
2022-10-28 22:40:28 +0200

    changed initializing message to embed and buttons

--- src/commands/initialize.js
+++ src/commands/initialize.js
@@ -1,10 +1,35 @@
+const {
+  EmbedBuilder,
+  ButtonBuilder,
+  ActionRowBuilder,
+  ButtonStyle,
+} = require("discord.js");
+
 module.exports = {
-    name: "initialize",
-    description: "Initializes bot",
-    cooldown: 1000 * 10,
-    async run(interaction) {
-      const message = await interaction.reply({ content: 'This is Payment Bot. Press icon below to pay' , fetchReply: true });
-      message.react('💵')
-    },
-  };
-  
+  name: "initialize",
+  description: "Initializes bot",
+  cooldown: 1000 * 10,
+  async run(interaction) {
+    // Create styled message
+    const embedMessage = new EmbedBuilder()
+      .setColor(0x0099ff)
+      .setTitle("Fast payment")
+      .setDescription("To start a payment react with 💸")
+      .setFooter({text: "Maślak3000 - discord payment bot"})
+
+    // Create buttons under message
+    const buttons = new ActionRowBuilder().addComponents(
+      new ButtonBuilder()
+        .setCustomId("payNow")
+        .setLabel("Pay now 💸")
+        .setStyle(ButtonStyle.Secondary)
+    );
+
+    // Reply to command, can also use
+    // interaction.channel.send
+    interaction.reply({
+      embeds: [embedMessage],
+      components: [buttons],
+    });
+  },
+};
